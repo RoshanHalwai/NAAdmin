f0e4f44d2065f5b366ded85ec8bca644ccada0d4
unknown <shivam6214@gmail.com>
2018-08-03 22:15:02 +0530

    Storing Carpenter and Electrician Data in Firebase

--- app/src/main/java/com/kirtanlabs/nammaapartmentssocietyservices/admin/Register.java
+++ app/src/main/java/com/kirtanlabs/nammaapartmentssocietyservices/admin/Register.java
@@ -2,7 +2,12 @@ package com.kirtanlabs.nammaapartmentssocietyservices.admin;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -22,13 +27,15 @@ import static com.kirtanlabs.nammaapartmentssocietyservices.Constants.FIREBASE_C
 import static com.kirtanlabs.nammaapartmentssocietyservices.Constants.SOCIETY_SERVICES_REFERENCE;
 import static com.kirtanlabs.nammaapartmentssocietyservices.Constants.SOCIETY_SERVICE_REGISTRATION_REQUEST_CODE;
 
-public class Register extends BaseActivity implements View.OnClickListener {
+public class Register extends BaseActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     /* ------------------------------------------------------------- *
      * Private Members
      * ------------------------------------------------------------- */
 
     private EditText editFullName, editMobileNumber;
+    private Spinner spinnerSocietyServiceType;
+    private String serviceType;
 
     /* ------------------------------------------------------------- *
      * Overriding BaseActivity Objects
@@ -60,7 +67,7 @@ public class Register extends BaseActivity implements View.OnClickListener {
         TextView textMobileNumber = findViewById(R.id.textMobileNumber);
         TextView textFullName = findViewById(R.id.textFullName);
         TextView textCountryCode = findViewById(R.id.textCountryCode);
-        Spinner spinnerSocietyServiceType = findViewById(R.id.spinnerSocietyServiceType);
+        spinnerSocietyServiceType = findViewById(R.id.spinnerSocietyServiceType);
         editFullName = findViewById(R.id.editFullName);
         editMobileNumber = findViewById(R.id.editMobileNumber);
         Button buttonRegister = findViewById(R.id.buttonRegister);
@@ -74,17 +81,29 @@ public class Register extends BaseActivity implements View.OnClickListener {
         editMobileNumber.setTypeface(Constants.setLatoRegularFont(this));
         buttonRegister.setTypeface(Constants.setLatoLightFont(this));
 
-        ServiceTypeAdapter serviceTypeAdapter = new ServiceTypeAdapter(Register.this, getResources().getStringArray(R.array.service_list));
+        /*Setting font for all the items in the list*/
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, getResources().getStringArray(R.array.service_list)) {
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView textServiceType = view.findViewById(android.R.id.text1);
+                textServiceType.setTypeface(Constants.setLatoRegularFont(Register.this));
+                return view;
+            }
+        };
 
         /*Setting adapter to Spinner view*/
-        spinnerSocietyServiceType.setAdapter(serviceTypeAdapter);
+        spinnerSocietyServiceType.setAdapter(adapter);
 
         /*Setting onClickListener for view*/
         buttonRegister.setOnClickListener(this);
+        spinnerSocietyServiceType.setOnItemSelectedListener(this);
     }
 
     /* ------------------------------------------------------------- *
-     * Overriding OnClickListeners Method
+     * Overriding OnClick and OnItemSelected Listeners Method
      * ------------------------------------------------------------- */
 
     @Override
@@ -94,6 +113,15 @@ public class Register extends BaseActivity implements View.OnClickListener {
         intent.putExtra(Constants.SCREEN_TITLE, R.string.register);
         intent.putExtra(Constants.SOCIETY_SERVICE_MOBILE_NUMBER, mobileNumber);
         startActivityForResult(intent, SOCIETY_SERVICE_REGISTRATION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        serviceType = spinnerSocietyServiceType.getItemAtPosition(position).toString().toLowerCase();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
     }
 
     /*-------------------------------------------------------------------------------
@@ -122,8 +150,7 @@ public class Register extends BaseActivity implements View.OnClickListener {
                 getString(R.string.please_wait_a_moment));
 
         /*Getting the reference of 'Data' child under 'societyServices'*/
-        //TODO: The 'societyServiceType' has been hardcoded to 'plumber' for now. Electrician & Carpenter will also be included.
-        DatabaseReference societyServicesReference = SOCIETY_SERVICES_REFERENCE.child("plumber").child(FIREBASE_CHILD_PRIVATE)
+        DatabaseReference societyServicesReference = SOCIETY_SERVICES_REFERENCE.child(serviceType).child(FIREBASE_CHILD_PRIVATE)
                 .child(FIREBASE_CHILD_DATA);
 
         /*Generating the societyServiceUID and creating a reference for it*/
@@ -139,9 +166,8 @@ public class Register extends BaseActivity implements View.OnClickListener {
         societyServicesAllReference.child(mobileNumber).setValue(societyServiceUID);
 
         /*Mapping UID with societyServiceType*/
-        //TODO: Change the service type here
         DatabaseReference societyTypeReference = Constants.SOCIETY_SERVICE_TYPE_REFERENCE.child(societyServiceUID);
-        societyTypeReference.child("plumber").setValue(true);
+        societyTypeReference.child(serviceType).setValue(true);
 
         /*Storing the Society Service personal details under societyServices->societyServiceType->data->private->societyServiceUID*/
         SocietyServiceData societyServiceData = new SocietyServiceData(fullName,
